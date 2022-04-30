50e27c6909bb34052e7e9024800d1c3edd79fc34
MarianaL19 <joscelynn7u7@gmail.com>
2022-04-29 21:38:33 -0500

    update: pop-up modificar del Formulario Vivienda
    Añadí la interfaz del pop-up al Formulario Vivienda, cuando se quiere modificar un registro (con el ícono de lápiz) se despliega.
    No he conectado el pop-up con la BD, no funciona.

--- FormularioVivienda/formularioVivienda.php
+++ FormularioVivienda/formularioVivienda.php
@@ -125,7 +125,8 @@
                             ?>
                             <th> <?php echo $municipio['nombre']?></th>
                             <th></th>
-                            <th> <a class="icons" href="actualizar.php?id=<?php echo $fila['id'] ?>"><i class="fa-solid fa-pencil"></i></a></th>
+                            <th> <a class="icons" href="#" id='btn-abrir-popup'><i class="fa-solid fa-pencil"></i></a></th>
+                            <!-- <th> <a class="icons" href="actualizar.php?id=<?php echo $fila['id'] ?>" id="btn-abrir-popup"><i class="fa-solid fa-pencil"></i></a></th> -->
                             <th> <a class="icons" href=""><i class="fa-solid fa-eye"></i></a></th>
                             <th> <a class="icons" href="eliminar.php?id=<?php echo $fila['id'] ?>"><i class="fa-solid fa-trash"></i></a></th>
                         </tr>
@@ -133,13 +134,6 @@
                         <?php
                             }
                         ?>
-
-                        <div class="overlay">
-                            <div class="popup">
-                            </div>
-                        </div>
-
-
                     </tbody>
                 </table>
             </section>
@@ -157,6 +151,32 @@
             </div>
         </div>
     </footer>
+
+    <div class="overlay" id="overlay">
+        <div class="popup" id="popup">
+            <a href="#" id="btn-cerrar-popup" class="btn-cerrar-popup"> <i class="fas fa-times"> </i> </a>
+            <h3>Editar registro</h3>
+            
+            <form action="">
+                <div class="contInputs">
+                    <input type="text" placeholder="Dirección">
+                    <input type="text" placeholder="Colonia">
+                    <select name="id_municipio" class="input">
+                        <?php
+                            while ($filaMunicipio = pg_fetch_array($consulta2)){
+                        ?>
+                        <option value="<?php echo $filaMunicipio['id']?>"> <?php echo $filaMunicipio['nombre']?> </option>
+                        <?php
+                            }
+                        ?>
+                    </select>
+                </div>
+                <input type="submit" class="btn-submit" value="Actualizar">
+            </form>
+        </div>
+    </div>
+
+    <script src="popup.js"></script>
 
 </body>
 
